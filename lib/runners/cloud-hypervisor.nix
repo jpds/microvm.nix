70c820cc9d9810731347db0af4283f1032620b9f
Jonathan Davies <jpds@protonmail.com>
2023-10-30 14:21:23 +0000

    Hardcode max_phys_bits=39 to cpus option for test.

--- lib/runners/cloud-hypervisor.nix
+++ lib/runners/cloud-hypervisor.nix
@@ -138,7 +138,7 @@ in {
          then "${pkgs.cloud-hypervisor-graphics}/bin/cloud-hypervisor"
          else "${pkgs.cloud-hypervisor}/bin/cloud-hypervisor"
         )
-        "--cpus" "boot=${toString vcpu}"
+        "--cpus" "boot=${toString vcpu},max_phys_bits=39"
         "--watchdog"
         "--console" "null"
         "--serial" "tty"
